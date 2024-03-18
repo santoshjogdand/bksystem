94ee1d3069a0d2a042f5d20a6c6240d63d458af6
Santosh Jogdand <santoshjogdand256@gmail.com>
2024-03-18 20:09:26 +0530

    Update transfer.php

--- pages/transfer.php
+++ pages/transfer.php
@@ -1,5 +1,10 @@
 <?php
 require 'connect.php';
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+
 $ok = 0;
 if (isset($_POST["submit"])) {
     session_start();
@@ -13,27 +18,25 @@ if (isset($_POST["submit"])) {
         $balq = "select current_bal from customers where name = '$paycust' and id = '$payeeid'";
         $bal = mysqli_query($conn, $balq);
 
-                        $balance = mysqli_fetch_assoc($bal);
-                        
-            if ($paycust != $benfcust) {
-                if ($paycust != "null" and $benfcust != "null" and $amount != "0") { 
+        $balance = mysqli_fetch_assoc($bal);
 
-                    if ($balance['current_bal'] >= $amount){
-                        $transfer1 = "UPDATE customers SET current_bal = current_bal - $amount WHERE name = '$paycust' and id = '$payeeid'";
-                        mysqli_query($conn, $transfer1);
-                        $transfer2 = "UPDATE customers SET current_bal = current_bal + $amount WHERE name = '$benfcust' and id = '$benfid'";
-                        mysqli_query($conn, $transfer2);
-                        $insert_query = "INSERT INTO transfers(id_payee, payee_name, id_benficiary, benf_name,amt) VALUES ($payeeid, '$paycust', $benfid, '$benfcust',$amount)";
-                        mysqli_query($conn, $insert_query);
-                        require 'success.php';
-                        $ok = 'success';
-                    }
-                    else{
+        if ($paycust != $benfcust) {
+            if ($paycust != "null" and $benfcust != "null" and $amount != "0") {
+                if ($balance['current_bal'] >= $amount) {
+                    $transfer1 = "UPDATE customers SET current_bal = current_bal - $amount WHERE name = '$paycust' and id = '$payeeid'";
+                    mysqli_query($conn, $transfer1);
+                    $transfer2 = "UPDATE customers SET current_bal = current_bal + $amount WHERE name = '$benfcust' and id = '$benfid'";
+                    mysqli_query($conn, $transfer2);
+                    $insert_query = "INSERT INTO transfers(id_payee, payee_name, id_benficiary, benf_name,amt) VALUES ($payeeid, '$paycust', $benfid, '$benfcust',$amount)";
+                    mysqli_query($conn, $insert_query);
+                    require 'success.php';
+                    $ok = 'success';
+                } else {
 
-                        $balance = mysqli_fetch_assoc($bal);
-                        require 'success.php';
-                        $ok = 'failed';
-                    }
+                    $balance = mysqli_fetch_assoc($bal);
+                    require 'success.php';
+                    $ok = 'failed';
+                }
             } else {
                 echo '<script>alert("Payee And Benficiary cant be same")</script>';
             }
@@ -54,7 +57,7 @@ if (isset($_POST["submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transfer</title>
     <link rel="stylesheet" href="../style/transfer.css">
-    
+
 </head>
 
 <body class="">
@@ -69,15 +72,15 @@ if (isset($_POST["submit"])) {
             <div class='mb-2'>
                 <h2 class="text-3xl text-start font-semibold">Send Money</h2>
                 <?php
-                    if ($ok == 'success') {
-                        echo $_SESSION["success"];
-                        session_unset();
-                    }
-                    if($ok == 'failed'){
-                        echo $_SESSION["insuf"];
-                        session_unset();
-                    }
-                    ?>
+                if ($ok == 'success') {
+                    echo $_SESSION["success"];
+                    session_unset();
+                }
+                if ($ok == 'failed') {
+                    echo $_SESSION["insuf"];
+                    session_unset();
+                }
+                ?>
             </div>
             <div class="payee">
                 <label class="text-base font-bold" for="payee">Payee Info: </label>
