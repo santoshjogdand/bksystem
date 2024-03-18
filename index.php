ed796de95be6d08a2dab3ca8dd1ff9b39dea06bf
Santosh Jogdand <santoshjogdand256@gmail.com>
2024-03-18 20:00:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,33 +8,35 @@
     <link rel="stylesheet" href="../style/header.css">
 
     <script src="https://cdn.tailwindcss.com"></script>
-    
-  <link rel="stylesheet" href="style/index.css">
+
+    <link rel="stylesheet" href="style/index.css">
 </head>
 
-<body>
-    <?php include("./comp/header.php"); ?>
-    <div class="flex justify-start items-center mt-[32px]">
-        <div class="flex flex-col ml-28">
-            <div class="text-2xl font-medium">
-                The Ultimate
-            </div>
-            <div class="flex text-5xl font-bold flex-col">
-                Banking Sytem Application
-                <div class="flex flex-row">
-                    <div class="bg-[#EA4C89] w-[9.5rem] h-2"></div>
-                    <!-- <div class="bg-[#EA4C89] w-[2.2rem] h-2 ml-[2rem]"></div>
+<body class="m-0 p-0 mx-0 overflow-x-hidden">
+
+    <!-- <div class="bg-[#EA4C89] w-[2.2rem] h-2 ml-[2rem]"></div>
                     <div class="bg-[#EA4C89] w-[8.6rem] h-2 ml-[1.5rem]"></div>
                     <div class="bg-[#EA4C89] w-[.6rem] h-2 ml-[1.4rem]"></div>
                     <div class="bg-[#EA4C89] w-[11rem] h-2 ml-[1.3rem]"></div> -->
+    <?php include("./comp/header.php"); ?>
+    <div class="flex md:ml-0 ml-3 md:flex-row flex-col z-10 justify-start items-center mt-[32px]">
+        <div class="flex flex-col ml-[3%]">
+            <div class="text-2xl texl-base font-medium">
+                The Ultimate
+            </div>
+            <div class="md:flex text-5xl font-bold flex-col">
+                <div class="flex flex-col ">
+                    Banking
+                    <span class="bg-[#EA4C89] md:w-[9.5rem] w-[9.5rem] h-1"></span>
                 </div>
+                Sytem Application
             </div>
-
-            <div class="mt-1">
-            Welcome to The Ultimate Banking Systems, Experience unparalleled convenience and peace of mind with our cutting-edge solutions.
+            <div class="mt-1 md:text-base md:ml-auto ml-1 text-sm">
+                Welcome to The Ultimate Banking Systems, Experience unparalleled convenience and peace of mind with our
+                cutting-edge solutions.
             </div>
-        </div>  
-        <div class='mr-10 animate-pulse'>
+        </div>
+        <div class='mr-10 -z-50 animate-pulse'>
             <img src="./bank.jpg" class="w-[750px]" alt="IMAGE NOT FOUND!!">
         </div>
     </div>
