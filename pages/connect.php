f9d7b36ca839af0ab0a493a3b4adfac5ab3ab7c9
Santosh Jogdand <santoshjogdand256@gmail.com>
2024-03-18 20:07:31 +0530

    Update connect.php

--- pages/connect.php
+++ pages/connect.php
@@ -3,6 +3,11 @@ $servername = "sql300.infinityfree.com";
 $username = "if0_36106232";
 $password = "cgDhZlmSPIo";
 $dbname = "if0_36106232_test1";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "test-1";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
